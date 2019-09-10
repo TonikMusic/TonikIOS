b7a99c80414e6c9bf585d0de9ea9860386a2d50f
Erick Wesley Espinoza <Wesleyespinoza1021@gmail.com>
2019-09-10 13:37:10 -0700

    Added Tab View with tabs

---
+++ Tonik/ViewControllers/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Tonik
+//
+//  Created by Wesley Espinoza on 8/29/19.
+//  Copyright © 2019 HazeWritesCode. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
